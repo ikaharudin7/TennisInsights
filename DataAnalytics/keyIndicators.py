a33317f3ff11094ef91d82ed09f5c5a42a065df8
ikaharudin7 <ikaharudin7@gmail.com>
2025-02-08 00:15:40 +1100

    feat: upload RG data

---
+++ DataAnalytics/keyIndicators.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import sklearn
+
+# Analyse what are the key statistics a player needs to win in a match to win on each of the 3 surfaces
+# Model be trained on top players historically over each surface, at a grand slam. 
+# Predictions will happen based on the inputs of key match statistics, based on player's averages for the last season on that surface. 
+
+# Clay Key Indicators
+
+
---
+++ MLModels/worldNumber1s.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+import sklearn
+
+# Create a ML model to build and predict the most important characteristics to be ranked 1 in the world. 
